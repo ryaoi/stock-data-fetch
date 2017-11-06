e3ad7581aadc3ecbccec851c31d69699a908584a
alice <alice@forever-am.com>
2017-11-06 16:45:50 +0100

    update badges

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # stock-data-fetch
+
+[![Build Status](https://travis-ci.org/forever-am/stock-data-fetch.svg?branch=master)](https://travis-ci.org/forever-am/stock-data-fetch)
+[![codecov](https://codecov.io/gh/forever-am/stock-data-fetch/branch/master/graph/badge.svg)](https://codecov.io/gh/forever-am/stock-data-fetch)
+[![Maintainability](https://api.codeclimate.com/v1/badges/c38475d15c3ebf96755e/maintainability)](https://codeclimate.com/repos/5a0082f9fd42915e00000983/maintainability)
+
+
 An exercise to fetch stock data from different sources with cache and sanity check.
 
 - You can check locally if the code respects pep8 by running the command `pep8 web` in `.`
--- web/api.py
+++ web/api.py
@@ -11,17 +11,19 @@ def mkdir_if_not_exist(dir_path):
 
 
 class DataReader(object):
+    start = "1926-01-01"
+
     def __init__(self, cache_dir=None):
         self.cache_dir = cache_dir or path.join(HOME_DIR, "stock-data")
         mkdir_if_not_exist(self.cache_dir)
 
-    def read(self, ticker, source, start="1990-01-01", end=None):
+    def read(self, ticker, source, end=None):
         end = end or datetime.now().date()
         filename = path.join(self.cache_dir, ticker + ".csv")
-        df = web.DataReader(ticker, source, start, end)
+        df = web.DataReader(ticker, source, self.start, end)
         df.to_csv(filename, header=True)
         return df
 
 
-def data_reader(ticker, source, start="1990-01-01", end=None):
-    return DataReader().read(ticker, source, start, end)
+def data_reader(ticker, source, end=None):
+    return DataReader().read(ticker, source, end)
